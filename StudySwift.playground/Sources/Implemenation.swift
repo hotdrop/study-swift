9844e977ce39f5d7a5b6806cd614b78094c88826
kenji <gunsecondstage0106@gmail.com>
2018-11-03 10:29:39 +0900

    StudyKotlinのsave the prisonerを書いた。switch文がとても強力なので見やすくかけたと思う

--- StudySwift.playground/Sources/Implemenation.swift
+++ StudySwift.playground/Sources/Implemenation.swift
@@ -5,12 +5,14 @@ public class Implementation: Study {
     public init() {}
     
     public func run() {
-        let exec = 2
+        let exec = 3
         switch exec {
         case 1:
             execBeautifulDaysAtTheMovies()
         case 2:
             execViralAdvertising()
+        case 3:
+            execSaveThePrisoner()
         default:
             print("nothing run code")
         }
@@ -68,5 +70,36 @@ public class Implementation: Study {
             return
         }
         print("Success")
+    }
+    
+    private func execSaveThePrisoner() {
+        let test1 = saveThePrisoner(n: 5, m: 2, s: 1)
+        if test1 != 2 {
+            print("case1 Failure...")
+            return
+        }
+        let test2 = saveThePrisoner(n: 5, m: 11, s: 1)
+        if test2 != 1 {
+            print("case2 Failure...")
+            return
+        }
+        let test3 = saveThePrisoner(n: 999999999, m: 999999999, s: 1)
+        if test3 != 999999999 {
+            print("case3 Failure...")
+            return
+        }
+        print("Success")
+    }
+    
+    private func saveThePrisoner(n: Int, m: Int, s: Int) -> Int {
+        switch (n, m, s) {
+        case (1, 1, _):
+            return s
+        case (_, _, 1) where n >= m:
+            return m
+        default:
+            let rem = (m % n) + (s - 1)
+            return (rem <= n) ? rem : rem - n
+        }
     }
 }
